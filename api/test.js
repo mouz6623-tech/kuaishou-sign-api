5af0ea48b3b158d5fcfdd3b674e9688ff49f6821
mouz6623-tech <mouz6623@gmail.com>
2026-02-11 15:01:49 +0800

    Delete api/test.js

--- api/test.js
+++
@@ -1,8 +0,0 @@
-// api/test.js - 最简单的测试
-module.exports = (req, res) => {
-  res.json({ 
-    message: 'Hello from Vercel Serverless!',
-    timestamp: new Date().toISOString(),
-    path: req.url
-  });
-};
